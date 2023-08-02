0620306968488b5636c9c2c772319e4450519c5c
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-08-02 17:44:34 +0000

    updated task_0

--- 0x03-React_component/task_0/dashboard/src/Notifications/NotificationItem.js
+++ 0x03-React_component/task_0/dashboard/src/Notifications/NotificationItem.js
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 function NotificationItem({ type, html, value }) {
   return (
     <>
-      {type && value ? <li key={type} data-notification-type={type}>{value}</li> : null}
+      {type && value ? <li data-notification-type={type}>{value}</li> : null}
       {html ? <li data-urgent dangerouslySetInnerHTML={{ __html: html }}></li> : null}
     </>
   );
