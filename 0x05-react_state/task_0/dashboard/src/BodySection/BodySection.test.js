c605e3fc496027426e6b56392257ba6f9fdd0c10
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-08-14 04:10:23 +0000

    0. Adding a local state for notifications

--- 0x05-react_state/task_0/dashboard/src/BodySection/BodySection.test.js
+++ 0x05-react_state/task_0/dashboard/src/BodySection/BodySection.test.js
@@ -1,1 +1,18 @@
-BodySection.js 
+import { shallow, mount } from 'enzyme';
+import React from 'react';
+import BodySection from './BodySection';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+describe('<BodySectionWithMarginBottom />', () => {
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
+  it(' renders BodySection without crashing', () => {
+    const wrapper = shallow(<BodySection />);
+    expect(wrapper.exists()).toEqual(true);
+  });
+});
