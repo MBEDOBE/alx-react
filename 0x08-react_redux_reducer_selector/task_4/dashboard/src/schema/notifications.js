8e3ef25f712d7702bbb4e13e7443ff9e74c8e35b
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-08-28 17:57:28 +0000

    Normalizr & Immutable

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/schema/notifications.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/schema/notifications.js
@@ -1,27 +1,38 @@
-import * as notificationData from "../../../../notifications.json";
-import { normalize, schema } from "normalizr";
+import * as notifications from '../../../../notifications.json';
+import { normalize, schema } from 'normalizr';
 
-const user = new schema.Entity("users");
-const message = new schema.Entity("messages", {}, { idAttribute: "guid" });
-const notification = new schema.Entity("notification", {
+export function getAllNotificationsByUser(userId) {
+  const notifications = normalizedNotifications.entities.notifications;
+  const messages = normalizedNotifications.entities.messages;
+  const selectedNotifs = [];
+
+  for (const property in notifications) {
+    if (notifications[property].author === userId) {
+      selectedNotifs.push(messages[notifications[property].context]);
+    }
+  }
+
+  return selectedNotifs;
+}
+
+export function notificationsNormalizer(data) {
+  return normalize(data, [notification]).entities;
+}
+
+const user = new schema.Entity('users');
+const message = new schema.Entity(
+  'messages',
+  {},
+  {
+    idAttribute: 'guid',
+  }
+);
+
+const notification = new schema.Entity('notifications', {
   author: user,
   context: message,
 });
 
-const normalized = normalize(notificationData, [notification]);
-
-export default function getAllNotificationsByUser(userId) {
-  const output = [];
-  const notifications = normalized.entities.notification;
-  const messages = normalized.entities.messages;
-
-  for (const id in notifications) {
-    if (notifications[id].author === userId) {
-      output.push(messages[notifications[id].context]);
-    }
-  }
-
-  return output;
-}
-
-export { normalized };
+export const normalizedNotifications = normalize(notifications.default, [
+  notification,
+]);
